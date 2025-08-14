28a3e8eb14c9df69875c22e78d0ef24f4e2a6171
KenRFH <riezqyken57@gmail.com>
2025-08-14 12:58:04 +0700

    kedua

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="fixed z-1000 bg-black/50 dark:bg-slate-800  dark:bg-opacity-30 backdrop-blur w-full p-4 text-white font-semibold">
+    <div class="flex justify-between items-center w-full max-w-7xl mx-auto">
+        <div class="text-lg font-bold">Logo</div>
+        <ul class="flex space-x-6">
+            <li><a href="/" class="hover:underline font-poppins">Home</a></li>
+            <li><a href="/" class="hover:underline">VR</a></li>
+            <li><a href="/" class="hover:underline">AR</a></li>
+        </ul>
+    </div>
+</nav>
+
+
